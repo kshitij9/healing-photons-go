2cd5350bbe110e7c40a83f79d230bc3b30f71ecd
Kshitij Tiwari <kshitijtwr@gmail.com>
2025-05-17 13:58:47 +0530

    corrected pieces model

--- internal/models/pieces.go
+++ internal/models/pieces.go
@@ -4,6 +4,6 @@ package models
 // MachineGrading represents the machine_grading table in the database
 type Pieces struct {
 	PieceID                int    `json:"piece_id"`
-	PieceCode              int    `json:"size_value"`
+	PieceCode              int    `json:"piece_code"`
 	Description			   string `json:"description"`
 }
