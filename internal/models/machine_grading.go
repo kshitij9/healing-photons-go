0d1a1cb26bfe541a7d5ac0807c0d0ad3889db300
Kshitij Tiwari <kshitijtwr@gmail.com>
2025-05-21 18:20:55 +0530

    minor

--- internal/models/machine_grading.go
+++ internal/models/machine_grading.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"encoding/json"
 	"time"
 )
 
@@ -15,4 +16,26 @@ type MachineGrading struct {
 	Weight            float64        `json:"weight"`
 	CreatedAt         time.Time      `json:"created_at"`
 	UpdatedAt         time.Time      `json:"updated_at"`
+}
+
+// UnmarshalJSON implements custom JSON unmarshaling for MachineGrading
+func (m *MachineGrading) UnmarshalJSON(data []byte) error {
+	type Alias MachineGrading
+	aux := &struct {
+		SizeVariationsID *int64 `json:"size_variations_id,omitempty"`
+		PiecesID         *int64 `json:"pieces_id,omitempty"`
+		*Alias
+	}{
+		Alias: (*Alias)(m),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	if aux.SizeVariationsID != nil {
+		m.SizeVariationsID = sql.NullInt64{Int64: *aux.SizeVariationsID, Valid: true}
+	}
+	if aux.PiecesID != nil {
+		m.PiecesID = sql.NullInt64{Int64: *aux.PiecesID, Valid: true}
+	}
+	return nil
 }
