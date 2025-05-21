c41184c776f5639e060326d52995b89d4647bd0c
Kshitij Tiwari <kshitijtwr@gmail.com>
2025-05-21 18:14:51 +0530

    made fields nullable

--- internal/models/machine_grading.go
+++ internal/models/machine_grading.go
@@ -1,17 +1,18 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
 // MachineGrading represents the machine_grading table in the database
 type MachineGrading struct {
-	ID                     string    `json:"id"`
-	ColorSortID            string    `json:"color_sort_id"`
-	StockID                string    `json:"stock_id"`
-	SizeVariationsID 	   int       `json:"size_variations_id"`
-	PiecesID			   int		 `json:"pieces_id"`	
-	Weight                 float64   `json:"weight"`
-	CreatedAt              time.Time `json:"created_at"`
-	UpdatedAt              time.Time `json:"updated_at"`
+	ID                string         `json:"id"`
+	ColorSortID       string         `json:"color_sort_id"`
+	StockID           string         `json:"stock_id"`
+	SizeVariationsID  sql.NullInt64  `json:"size_variations_id,omitempty"`
+	PiecesID          sql.NullInt64  `json:"pieces_id,omitempty"`
+	Weight            float64        `json:"weight"`
+	CreatedAt         time.Time      `json:"created_at"`
+	UpdatedAt         time.Time      `json:"updated_at"`
 }
